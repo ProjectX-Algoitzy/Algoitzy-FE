19e9bc75682d97d7316188d8594df6c16fc557f9
JWMin556 <117150131+JWMin556@users.noreply.github.com>
2024-05-01 13:39:07 +0900

    [BIGERROR] request js의 baseURL 변경 #37

--- src/APP/Api/request.js
+++ src/APP/Api/request.js
@@ -6,7 +6,7 @@ export const REFRESH_TOKEN = 'refreshToken';
 //앞으로 토큰은 여기서 사용하시면 됩니다.
 // Authorization 에 토큰 자동으로 들어가도록 설정하였습니다.
 const request = axios.create({
-  baseURL: 'http://3.35.47.250:8281',
+  baseURL: 'http://13.209.12.121:8281',
   headers: {
     withCredentials: true,
     transformRequest: true,

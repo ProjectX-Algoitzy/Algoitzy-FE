4c12a99edf39df5646e3d2c80e37591e1299d2f5
nunomi0 <ykys1@naver.com>
2024-08-14 05:47:02 +0900

    [DESIGN] FindAuth PX->REM

--- src/APP/user-pages/FindAuth/FindEmailSuccess/Styled/FindAuth.FindEmailSuccess.styles.js
+++ src/APP/user-pages/FindAuth/FindEmailSuccess/Styled/FindAuth.FindEmailSuccess.styles.js
@@ -6,7 +6,6 @@ export const Container = styled.div`
   display: flex;
   justify-content: center; 
   background-image: url('/img/signup.png');
-  
 `;
 
 export const InnerContainer = styled.div`
@@ -14,16 +13,16 @@ export const InnerContainer = styled.div`
   flex-direction: column;
   align-items: center; 
   background-color: ${tokens.colors.White};
-  border: 1px solid ${tokens.colors.Grey_4};
-  border-radius: 20px;
-  padding: 128px 305px;
-  margin-top: 60px;
+  border: 0.042rem solid ${tokens.colors.Grey_4};
+  border-radius: 0.83rem;
+  padding: 5.33rem 12.71rem;
+  margin-top: 2.5rem;
 `;
 
 // 아이디 찾기 head
 export const Head3 = styled.div`
 	${tokens.typography.H3_SB_40}
-	margin-bottom: 108px;
+	margin-bottom: 4.5rem;
 `;
 
 // 라벨+입력 컨테이너
@@ -36,16 +35,16 @@ export const LIContainer = styled.div`
 export const Body = styled.div`
 	${tokens.typography.T4_SB_20};
 	color: ${tokens.colors.Grey_8};
-	width: 590px;	
-	margin-bottom: 10px;
+	width: 24.58rem;
+	margin-bottom: 0.42rem;
 `;
 
 // text
 export const Text = styled.div`
 	${tokens.typography.B3_M_14};
 	color: ${tokens.colors.Grey_6};
-	width: 590px;	
-	margin-bottom: 16px;
+	width: 24.58rem;
+	margin-bottom: 0.67rem;
 `;
 
 // 이메일 박스
@@ -53,25 +52,25 @@ export const EmailBox = styled.div`
 	display: flex;
 	flex-direction: row;
 	align-items: center;
-	border: 1px solid ${tokens.colors.B_Grey_6};
-	border-radius: 4px;
-	width: 590px;	
-	margin-bottom: 256px;
+	border: 0.042rem solid ${tokens.colors.B_Grey_6};
+	border-radius: 0.17rem;
+	width: 24.58rem;
+	margin-bottom: 10.67rem;
 `;
 
 // 이메일 아이콘
 export const EmailIcon = styled.div`
-	border: 1px solid ${tokens.colors.B_Grey_6};
-	border-radius: 4px;
-	width: 24px;
-	height: 24px;	
-	margin: 16px;
+	border: 0.042rem solid ${tokens.colors.B_Grey_6};
+	border-radius: 0.17rem;
+	width: 1rem;
+	height: 1rem;
+	margin: 0.67rem;
 `;
 
 // 이메일 계정
 export const Email = styled.div`
 	${tokens.typography.B3_M_14};
-	height: 18px;
+	height: 0.75rem;
 `;
 
 // 버튼 컨테이너
@@ -83,20 +82,18 @@ export const BtnContainer = styled.div`
 // 로그인 버튼
 export const BtnLogin = styled.button`
 	${tokens.Btns.Btn_fill_default};
-    width: 287px;
+	width: 11.96rem;
 	color: ${tokens.colors.White};
 	${tokens.typography.T4_SB_20};
-    margin-right: 8px;
-	
+	margin-right: 0.33rem;
 `;
 
 // 비밀번호 찾기 버튼
 export const BtnFindPassword = styled.button`
 	${tokens.Btns.Btn_fill_default};
 	background-color: ${tokens.colors.B_Grey_6};
-	width: 287px;
+	width: 11.96rem;
 	color: ${tokens.colors.White};
 	${tokens.typography.T4_SB_20};
-	margin-left: 8px;
-	
+	margin-left: 0.33rem;
 `;
--- src/APP/user-pages/FindAuth/FindPassword/Styled/FindPassword.findpassword.styles.js
+++ src/APP/user-pages/FindAuth/FindPassword/Styled/FindPassword.findpassword.styles.js
@@ -6,7 +6,6 @@ export const Container = styled.div`
   display: flex;
   justify-content: center; 
   background-image: url('/img/signup.png');
-  
 `;
 
 export const InnerContainer = styled.div`
@@ -14,16 +13,16 @@ export const InnerContainer = styled.div`
   flex-direction: column;
   align-items: center; 
   background-color: ${tokens.colors.White};
-  border: 1px solid ${tokens.colors.Grey_4};
-  border-radius: 20px;
-  padding: 128px 305px;
-  margin-top: 60px;
+  border: 0.042rem solid ${tokens.colors.Grey_4};
+  border-radius: 0.83rem;
+  padding: 5.33rem 12.71rem;
+  margin-top: 2.5rem;
 `;
 
 // 아이디 찾기 head
 export const Head3 = styled.div`
 	${tokens.typography.H3_SB_40}
-	margin-bottom: 78px;
+	margin-bottom: 3.25rem;
 `;
 
 // 라벨+입력 컨테이너
@@ -34,23 +33,23 @@ export const LIContainer = styled.div`
 
 // 입력값 라벨
 export const Label = styled.label`
-	height: 24px;
+	height: 1rem;
 	${tokens.typography.T4_SB_20}
 	color: ${tokens.colors.Grey_8};
-	margin-top: 30px;
-	margin-bottom: 10px;
+	margin-top: 1.25rem;
+	margin-bottom: 0.42rem;
 `;
 
 // 입력 박스
 export const InputBox = styled.input`
-	width: 570px;
-	height: 54.4px;
-	border: 1px solid ${tokens.colors.Grey_4};
-	border-radius: 4px;
+	width: 23.75rem;
+	height: 2.27rem;
+	border: 0.042rem solid ${tokens.colors.Grey_4};
+	border-radius: 0.17rem;
 	${tokens.typography.B2_M_16}
 	color: ${tokens.colors.Grey_8};
-	padding: 0 10px;
-	margin-bottom: 4px;
+	padding: 0 0.42rem;
+	margin-bottom: 0.17rem;
 
 	&::placeholder {
 		color: ${tokens.colors.Grey_4}; 
@@ -58,9 +57,8 @@ export const InputBox = styled.input`
 
 	// 텍스트 입력되어 있을 경우 border 색상변경
 	&:not(:placeholder-shown) { 
-		border: 1px solid ${tokens.colors.Grey_6};
-	  }
-	
+		border: 0.042rem solid ${tokens.colors.Grey_6};
+	}
 `;
 
 // 입력+인증 컨테이너
@@ -71,14 +69,14 @@ export const InputConfirmBoxWrapper = styled.div`
 
 // 입력+인증 박스
 export const InputConfirmBox = styled.input`
-	width: 414px;
-	height: 54.4px;
-	border: 1px solid ${tokens.colors.Grey_4};
-	border-radius: 4px;
+	width: 17.25rem;
+	height: 2.27rem;
+	border: 0.042rem solid ${tokens.colors.Grey_4};
+	border-radius: 0.17rem;
 	${tokens.typography.B2_M_16}
 	color: ${tokens.colors.Grey_8};
-	padding: 0 10px;
-	margin-bottom: 4px;
+	padding: 0 0.42rem;
+	margin-bottom: 0.17rem;
 
 	&::placeholder {
 		color: ${tokens.colors.Grey_4}; 
@@ -86,8 +84,8 @@ export const InputConfirmBox = styled.input`
 
 	// 텍스트 입력되어 있을 경우 border 색상변경
 	&:not(:placeholder-shown) { 
-		border: 1px solid ${tokens.colors.Grey_6};
-	  }
+		border: 0.042rem solid ${tokens.colors.Grey_6};
+	}
 
 	// 텍스트 입력되어 있을 경우 해당 InputConfirmBox 구성 요소 바로 뒤에 오는 버튼 색상 변경
 	&:not(:placeholder-shown) + button {
@@ -100,9 +98,8 @@ export const Btn = styled.button`
 	${tokens.Btns.Btn_fill_default}
 	color: ${tokens.colors.White};
 	${tokens.typography.T4_SB_20}
-	margin-top: 72px;
-	margin-bottom: 45px;
-	
+	margin-top: 3rem;
+	margin-bottom: 1.88rem;
 `;
 
 // 인증하기 버튼
@@ -110,22 +107,22 @@ export const BtnConfirm = styled.button`
 	${tokens.Btns.Btn_fill_certi_disable}
 	color: ${tokens.colors.White};
 	${tokens.typography.B2_M_16}
-	margin-left: 16px;
-	margin-bottom: 4px;
+	margin-left: 0.67rem;
+	margin-bottom: 0.17rem;
 `;
 
 // 입력값 설명
 export const CodeMessage = styled.div`
 	color: ${tokens.colors.Grey_6};
 	${tokens.typography.B3_M_14}
-	margin-bottom: 14px;
+	margin-bottom: 0.58rem;
 `;
 
 // 에러 메시지
 export const ErrorMessage = styled.div`
 	color: ${tokens.colors.Red};
 	${tokens.typography.B3_M_14}
-	margin-bottom: 14px;
+	margin-bottom: 0.58rem;
 `;
 
 // 백준 계정 인증 확인 메시지

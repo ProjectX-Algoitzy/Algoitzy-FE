8541ece619d9dc4332bd150d68b4799bf4e48fd8
begojara99 <pch990824@naver.com>
2024-09-04 14:28:19 +0900

    [DESIGN] 아이디 찾기, 비밀번호 찾기 분리

--- src/APP/user-pages/Auth/Auth.login.jsx
+++ src/APP/user-pages/Auth/Auth.login.jsx
@@ -382,8 +382,9 @@ export default function Login() {
             <itemS.RouteSignup onClick={() => navigate("/signup")}>
               회원가입
             </itemS.RouteSignup>
-            <itemS.UtilText onClick={() => navigate("/findemail")}>아이디</itemS.UtilText>
-            <itemS.UtilText>/</itemS.UtilText>
+            <itemS.UtilText>|</itemS.UtilText>
+            <itemS.UtilText onClick={() => navigate("/findemail")}>아이디 찾기</itemS.UtilText>
+            <itemS.UtilText>|</itemS.UtilText>
             <itemS.UtilText onClick={() => navigate("/findpassword")}>
               비밀번호 찾기
             </itemS.UtilText>

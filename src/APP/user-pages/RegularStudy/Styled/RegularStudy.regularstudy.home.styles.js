466885c819534e18e7b1c10c72070b2117eeacb4
nunomi0 <ykys1@naver.com>
2024-09-04 17:37:30 +0900

    [DESIGN] 정규스터디 홈 html 글자 크기 조정

--- src/APP/user-pages/RegularStudy/Styled/RegularStudy.regularstudy.home.styles.js
+++ src/APP/user-pages/RegularStudy/Styled/RegularStudy.regularstudy.home.styles.js
@@ -29,6 +29,7 @@ export const ContentContainer = styled.div`
   width: 100%;
   height: 100%;
   margin-bottom: 4.21rem;
+  font-size: 0.75rem; /* 24px 기준 18px 크기, 에디터 수정 시 비율 조정 필요 */
 `;
 
 export const BlueContainer = styled.div`

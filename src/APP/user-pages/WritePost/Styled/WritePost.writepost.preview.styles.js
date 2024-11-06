f1193f355b8ca8554d7f27390416d3f6806d9f20
nunomi0 <ykys1@naver.com>
2024-11-06 18:17:28 +0900

    에디터 preview 글씨 색 수정

--- src/APP/user-pages/WritePost/Styled/WritePost.writepost.preview.styles.js
+++ src/APP/user-pages/WritePost/Styled/WritePost.writepost.preview.styles.js
@@ -8,21 +8,20 @@ export const PreviewContainer = styled.div`
   background-color: #F5FBFF;
   font-family: 'Pretendard', sans-serif;
   font-size: 1rem;
+  color: ${tokens.colors.B_Grey_8}; /* 텍스트 기본 색상 설정 */
   line-height: 1.6;
   overflow-y: auto;
   height: 100%;
   word-wrap: break-word; /* 줄바꿈 설정 */
   white-space: pre-wrap; /* 긴 문자열이 줄바꿈되도록 설정 */
-  
+
   h1 {
     font-size: 2rem;
     margin-top: 0;
-    color: #333;
   }
   h2 {
     font-size: 1.75rem;
     margin-top: 1rem;
-    color: #333;
   }
   p {
     margin: 1rem 0;

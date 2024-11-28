0d4368a953e9e8c4f97367da26ec5206a247908b
nunomi0 <ykys1@naver.com>
2024-11-28 18:44:54 +0900

    임시저장 모달 토글 수정

--- src/APP/user-pages/WritePost/WritePost.writepost.draft.jsx
+++ src/APP/user-pages/WritePost/WritePost.writepost.draft.jsx
@@ -14,7 +14,10 @@ export default function DraftModal({ isDraftModalOpen, toggleDraftModal, drafts,
         <Styled.DraftModalBody>
           {drafts.length > 0 ? (
             drafts.map((draft) => (
-              <Styled.DraftItem key={draft.boardId} onClick={() => onSelectDraft(draft)}>
+              <Styled.DraftItem key={draft.boardId}
+              onClick={() => {
+                toggleDraftModal();
+                onSelectDraft(draft)}}>
                 <Styled.DraftTitle>{draft.title}</Styled.DraftTitle>
                 <Styled.DraftDate>{new Date(draft.createdTime).toLocaleString()}</Styled.DraftDate>
               </Styled.DraftItem>

b9ba37ab365223dd1ccbc0100a129ffbf9cb1ca7
begojara99 <pch990824@naver.com>
2025-02-22 16:33:17 +0900

    자율 스터디 대표이미지 업로드 제한 주석처리

--- src/APP/user-pages/MakingSelfStudy/MakingSelfStudy.makingselfstudy.jsx
+++ src/APP/user-pages/MakingSelfStudy/MakingSelfStudy.makingselfstudy.jsx
@@ -27,9 +27,8 @@
 //     fileInputRef.current.click();
 //   };
 
-
 //   return (
-    
+
 //       <itemS.Container>
 //         <itemS.InnerContainer>
 //           <itemS.StudyNameContainer>
@@ -92,25 +91,24 @@
 //         </itemS.InnerContainer>
 
 //       </itemS.Container>
-    
+
 //   );
 // };
 
 // export default MakingSelfStudy;
 
-
-import React, { useCallback, useContext, useState } from 'react'
+import React, { useCallback, useContext, useState } from "react";
 import * as itemS from "./Styled/MakingSelfStudy.makingselfstudy.styles";
-import request from '../../Api/request';
-import QuilEditor from '../../components/Editor/Editor.quileditor';
-import { useDropzone } from 'react-dropzone';
-import { useNavigate } from 'react-router-dom';
-import { AlertContext } from '../../Common/Alert/AlertContext';
+import request from "../../Api/request";
+import QuilEditor from "../../components/Editor/Editor.quileditor";
+import { useDropzone } from "react-dropzone";
+import { useNavigate } from "react-router-dom";
+import { AlertContext } from "../../Common/Alert/AlertContext";
 
 export default function MakingSelfStudy() {
   const navigate = useNavigate();
-  const [name, setName] = useState('');
-  const [content, setContent] = useState('');
+  const [name, setName] = useState("");
+  const [content, setContent] = useState("");
   const [imageUrl, setImageUrl] = useState(null);
   const { alert } = useContext(AlertContext);
 
@@ -121,45 +119,65 @@ export default function MakingSelfStudy() {
   const uploadImage = async (file) => {
     try {
       const formData = new FormData();
-      formData.append('multipartFileList', file);
+      formData.append("multipartFileList", file);
 
-      const response = await request.post('/s3', formData);
+      const response = await request.post("/s3", formData);
       if (response.isSuccess) {
-          console.log("업로드된 이미지 URL:", response.result[0]); // 콘솔에 URL 출력
-          return response.result[0]; // 반환된 이미지 URL
+        console.log("업로드된 이미지 URL:", response.result[0]); // 콘솔에 URL 출력
+        return response.result[0]; // 반환된 이미지 URL
       } else {
-          throw new Error('이미지 업로드 실패');
+        throw new Error("이미지 업로드 실패");
       }
     } catch (error) {
-      console.error('이미지 업로드 오류:', error);
+      console.error("이미지 업로드 오류:", error);
       throw error;
     }
   };
 
   const FileUpload = ({ onFileUpload }) => {
-    const onDrop = useCallback(async (acceptedFiles) => {
+    const onDrop = useCallback(
+      async (acceptedFiles) => {
         const uploadedFile = acceptedFiles[0];
         try {
-            const url = await uploadImage(uploadedFile);
-            setImageUrl(url); // 이미지 URL을 상태에 저장
-            onFileUpload(url); // 부모 컴포넌트에 URL 전달
+          const url = await uploadImage(uploadedFile);
+          setImageUrl(url); // 이미지 URL을 상태에 저장
+          onFileUpload(url); // 부모 컴포넌트에 URL 전달
         } catch (error) {
-            console.error("파일 업로드 중 오류가 발생했습니다.", error);
+          console.error("파일 업로드 중 오류가 발생했습니다.", error);
         }
-    }, [onFileUpload]);
+      },
+      [onFileUpload]
+    );
 
-    const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+    // const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    //   onDrop,
+    //   accept: ["image/jpeg", "image/png", "image/gif", "image/svg+xml"],
+    //   multiple: false,
+    // });
+    const { getRootProps, getInputProps, isDragActive } = useDropzone({
+      onDrop,
+      multiple: false, // 여러 개 업로드 방지
+    });
 
     return (
-        <itemS.FileUploadContainer {...getRootProps()} className={isDragActive ? 'dragActive' : ''} >
-            <input {...getInputProps()} />
-            {!imageUrl && <p>이미지 드래그 혹은 <itemS.HighlightText>파일 업로드</itemS.HighlightText></p>}
-            {imageUrl && (
-                <itemS.ImagePreview>
-                    <img src={imageUrl} alt="Preview" />
-                </itemS.ImagePreview>
-            )}
-        </itemS.FileUploadContainer>
+      <itemS.FileUploadContainer
+        {...getRootProps()}
+        className={isDragActive ? "dragActive" : ""}
+      >
+        {/* <input {...getInputProps()} accept=".jpg, .jpeg, .png, .gif, .svg" /> */}
+        <input {...getInputProps()} />
+        {!imageUrl && (
+          <p>
+            이미지 드래그 혹은{" "}
+            <itemS.HighlightText>파일 업로드</itemS.HighlightText>
+          </p>
+        )}
+        {imageUrl && (
+          <itemS.ImagePreview>
+            <img src={imageUrl} alt="Preview" />
+          </itemS.ImagePreview>
+        )}
+      </itemS.FileUploadContainer>
     );
   };
 
@@ -167,48 +185,52 @@ export default function MakingSelfStudy() {
     const requestData = {
       profileUrl: imageUrl,
       name: name,
-      content: content
+      content: content,
     };
 
     try {
-      const response = await request.post('/study', requestData)
+      const response = await request.post("/study", requestData);
       console.log("자율 스터디 생성 성공: ", response);
 
       if (response["isSuccess"]) {
         await alert("자율 스터디가 생성되었습니다.");
         navigate("/study");
-      } 
+      }
     } catch (error) {
-      console.error('자율스터디 저장과정에서 에러', error);
+      console.error("자율스터디 저장과정에서 에러", error);
     }
   };
 
-
   return (
     <itemS.BackGroundContainer>
-        <itemS.Container>
-            <itemS.StyledPageName>새로운 스터디</itemS.StyledPageName>
-            <itemS.ContentContainer>
-                <itemS.LittleContainer>
-                    <itemS.StyledTitle>스터디 대표 이미지</itemS.StyledTitle>
-                    <FileUpload onFileUpload={(url) => setImageUrl(url)}/>
-                </itemS.LittleContainer>
+      <itemS.Container>
+        <itemS.StyledPageName>새로운 스터디</itemS.StyledPageName>
+        <itemS.ContentContainer>
+          <itemS.LittleContainer>
+            <itemS.StyledTitle>스터디 대표 이미지</itemS.StyledTitle>
+            <FileUpload onFileUpload={(url) => setImageUrl(url)} />
+          </itemS.LittleContainer>
 
-                <itemS.LittleContainer>
-                    <itemS.StyledTitle>이름</itemS.StyledTitle>
-                    <itemS.StyledInput placeholder='이름을 입력해주세요' type='text' value={name} onChange={onChangeName} />
-                </itemS.LittleContainer>
+          <itemS.LittleContainer>
+            <itemS.StyledTitle>이름</itemS.StyledTitle>
+            <itemS.StyledInput
+              placeholder="이름을 입력해주세요"
+              type="text"
+              value={name}
+              onChange={onChangeName}
+            />
+          </itemS.LittleContainer>
 
-                <itemS.LittleContainer>
-                    <itemS.StyledTitle>내용</itemS.StyledTitle>
-                    <QuilEditor setContent={setContent} />
-                </itemS.LittleContainer>
+          <itemS.LittleContainer>
+            <itemS.StyledTitle>내용</itemS.StyledTitle>
+            <QuilEditor setContent={setContent} />
+          </itemS.LittleContainer>
 
-                <itemS.LittleContainer style={{alignItems:"center"}}>
-                    <itemS.Btn onClick={handleSave}>확인</itemS.Btn>
-                </itemS.LittleContainer>
-            </itemS.ContentContainer>
-        </itemS.Container>
+          <itemS.LittleContainer style={{ alignItems: "center" }}>
+            <itemS.Btn onClick={handleSave}>확인</itemS.Btn>
+          </itemS.LittleContainer>
+        </itemS.ContentContainer>
+      </itemS.Container>
     </itemS.BackGroundContainer>
-  )
+  );
 }

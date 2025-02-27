f9748dac71cd8af94df1c7e6953e74aa5abced58
begojara99 <pch990824@naver.com>
2025-02-27 16:06:10 +0900

    [FEATURE] 마이페이지 문의하기 탭 추가 #375

--- src/APP/user-pages/Mypage/Mypage.mypage.myinquiry.tuple.jsx
+++ src/APP/user-pages/Mypage/Mypage.mypage.myinquiry.tuple.jsx
@@ -1,27 +1,32 @@
-import React, { useEffect, useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import * as itemS from "./Styled/Mypage.mypage.myboard.tuple.styles";
 
-export default function MyInquiryTuple({ item, isChecked, onCheckChange, isMemberMatch }) {
+export default function MyInquiryTuple({
+  item,
+  isChecked,
+  onCheckChange,
+  isMemberMatch,
+}) {
   const navigate = useNavigate();
 
   const formatDate = (createdTime) => {
     const date = new Date(createdTime);
     const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
     return `${year}.${month}.${day}`;
   };
 
   const truncateTitle = (name) => {
-		if (name.length > 64) {
-			return name.slice(0, 63) + '...';
-		}
-		return name;
-	}
+    if (name.length > 64) {
+      return name.slice(0, 63) + "...";
+    }
+    return name;
+  };
 
   const moveToDetail = (id) => {
-    // navigate(`/inquiry/${id}`);
+    navigate(`/inquiryboard/${id}`);
   };
 
   return (
@@ -40,20 +45,19 @@ export default function MyInquiryTuple({ item, isChecked, onCheckChange, isMembe
       )}
       <>
         <itemS.TupleId>{item.categoryName}</itemS.TupleId>
-          <itemS.TupleTitleBox onClick={() => moveToDetail(item.inquiryId)}>
-              <itemS.TupleTitle data-delete-yn={undefined}>
-                {truncateTitle(item.title)}
-              </itemS.TupleTitle>
-            </itemS.TupleTitleBox>
+        <itemS.TupleTitleBox onClick={() => moveToDetail(item.inquiryId)}>
+          <itemS.TupleTitle data-delete-yn={undefined}>
+            {truncateTitle(item.title)}
+          </itemS.TupleTitle>
+        </itemS.TupleTitleBox>
         <itemS.TupleDate>{formatDate(item.createdTime)}</itemS.TupleDate>
         <itemS.TupleView>{item.viewCount} 회</itemS.TupleView>
         <itemS.TupleProcess>
           <itemS.ProcessingYNBox solvedYn={item.solvedYn}>
-            {item.solvedYn ? '답변 완료' : '답변 대기'}
+            {item.solvedYn ? "답변 완료" : "답변 대기"}
           </itemS.ProcessingYNBox>
         </itemS.TupleProcess>
       </>
-      
     </itemS.TupleContainer>
   );
 }

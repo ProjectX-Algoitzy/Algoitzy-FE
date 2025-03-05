055e771cb8c2be62523c1bcb9b5b8fe17a5ed2d5
begojara99 <pch990824@naver.com>
2025-03-05 17:47:42 +0900

    [FEATURE] 마이페이지 문의하기 탭 글쓰기 버튼 #403

--- src/APP/user-pages/Mypage/Mypage.mypage.myinquiry.table.jsx
+++ src/APP/user-pages/Mypage/Mypage.mypage.myinquiry.table.jsx
@@ -1,11 +1,16 @@
-import React, { useEffect, useState, useRef, useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
-import MyInquiryTuple from './Mypage.mypage.myinquiry.tuple';
+import React, { useEffect, useState, useRef, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import MyInquiryTuple from "./Mypage.mypage.myinquiry.tuple";
 import * as itemS from "./Styled/Mypage.mypage.myboard.table.styles";
-import request from '../../Api/request';
-import { AlertContext } from '../../Common/Alert/AlertContext';
+import request from "../../Api/request";
+import { AlertContext } from "../../Common/Alert/AlertContext";
 
-export default function MyInquiryTable({ items, inquiryCount, isMemberMatch, fetchinquiry={fetchinquiry} }) {
+export default function MyInquiryTable({
+  items,
+  inquiryCount,
+  isMemberMatch,
+  fetchinquiry = { fetchinquiry },
+}) {
   const navigate = useNavigate();
   const { alert } = useContext(AlertContext);
 
@@ -15,7 +20,7 @@ export default function MyInquiryTable({ items, inquiryCount, isMemberMatch, fet
   const [isAllChecked, setIsAllChecked] = useState({
     inquiry: false, // 게시한 글의 전체 선택 상태
   });
-  
+
   const [count, setCount] = useState(inquiryCount); //TODO -  - 임시로 10 넣음
 
   // 게시글, 임시저장글 탭 변경
@@ -30,11 +35,14 @@ export default function MyInquiryTable({ items, inquiryCount, isMemberMatch, fet
   const handleScrollSync = (e) => {
     const scrollable = e.target;
     const syncScroll =
-      scrollable === contentRef.current ? scrollRef.current : contentRef.current;
+      scrollable === contentRef.current
+        ? scrollRef.current
+        : contentRef.current;
 
     if (syncScroll) {
       const scrollRatio =
-        scrollable.scrollTop / (scrollable.scrollHeight - scrollable.clientHeight);
+        scrollable.scrollTop /
+        (scrollable.scrollHeight - scrollable.clientHeight);
 
       if (scrollable === contentRef.current) {
         setThumbTop(scrollRatio * (scrollRef.current.clientHeight - 96)); // Thumb 위치 업데이트
@@ -55,7 +63,8 @@ export default function MyInquiryTable({ items, inquiryCount, isMemberMatch, fet
     );
 
     setThumbTop(newTop);
-    contentRef.current.scrollTop = (newTop / thumbHeight) * (scrollableHeight - containerHeight);
+    contentRef.current.scrollTop =
+      (newTop / thumbHeight) * (scrollableHeight - containerHeight);
   };
 
   const handleCheckChange = (inquiryId) => {
@@ -67,20 +76,19 @@ export default function MyInquiryTable({ items, inquiryCount, isMemberMatch, fet
       },
     }));
   };
-  
+
   const handleAllCheckChange = () => {
     const newIsAllChecked = !isAllChecked[selectedTab];
     setIsAllChecked((prev) => ({
       ...prev,
       [selectedTab]: newIsAllChecked,
     }));
-  
+
     const newCheckedItems = {};
-    items
-      .forEach((item) => {
-        newCheckedItems[item.inquiryId] = newIsAllChecked;
-      });
-  
+    items.forEach((item) => {
+      newCheckedItems[item.inquiryId] = newIsAllChecked;
+    });
+
     setCheckedItems((prev) => ({
       ...prev,
       [selectedTab]: newCheckedItems,
@@ -91,12 +99,12 @@ export default function MyInquiryTable({ items, inquiryCount, isMemberMatch, fet
     const idsToDelete = Object.keys(items[selectedTab]).filter(
       (id) => items[selectedTab][id] === true
     );
-  
+
     if (idsToDelete.length === 0) {
       alert("삭제할 게시글을 선택해주세요.");
       return;
     }
-  
+
     try {
       for (let id of idsToDelete) {
         const response = await request.delete(`/inquiry/${id}`);
@@ -106,7 +114,7 @@ export default function MyInquiryTable({ items, inquiryCount, isMemberMatch, fet
           console.error(`게시글 ${id} 삭제 실패:`, response);
         }
       }
-  
+
       fetchinquiry();
       setCheckedItems((prev) => ({
         ...prev,
@@ -120,21 +128,17 @@ export default function MyInquiryTable({ items, inquiryCount, isMemberMatch, fet
       console.error("삭제 처리 중 오류 발생:", error);
     }
   };
-  
-
 
   const handleWriteClick = () => {
-		navigate('/writepost'); 
-	};
+    navigate("/writeinquiry");
+  };
 
   return (
     <itemS.Container>
       <itemS.Table>
         <itemS.TabBtnContainer>
           <itemS.TabBox>
-            <itemS.Tab>
-              문의하기 글 {inquiryCount}
-            </itemS.Tab>
+            <itemS.Tab>문의하기 글 {inquiryCount}</itemS.Tab>
           </itemS.TabBox>
         </itemS.TabBtnContainer>
         <itemS.TableContainerWrapper>
@@ -156,24 +160,28 @@ export default function MyInquiryTable({ items, inquiryCount, isMemberMatch, fet
                     아직 작성된 글이 없습니다.
                   </itemS.NoItemsContainer>
                 ) : (
-                  items 
-                    .map((item) => (
-                      <MyInquiryTuple
-                        key={item.inquiryId}
-                        item={item}
-                        isChecked={checkedItems[selectedTab][item.inquiryId] || false}
-                        onCheckChange={() => handleCheckChange(item.inquiryId)}
-                        isMemberMatch={isMemberMatch}
-                      />
-                    ))
+                  items.map((item) => (
+                    <MyInquiryTuple
+                      key={item.inquiryId}
+                      item={item}
+                      isChecked={
+                        checkedItems[selectedTab][item.inquiryId] || false
+                      }
+                      onCheckChange={() => handleCheckChange(item.inquiryId)}
+                      isMemberMatch={isMemberMatch}
+                    />
+                  ))
                 )}
               </itemS.TupleContainer>
             </itemS.TupleContainerWrapper>
           </itemS.TableContainer>
-        
-          {count > 8 &&
+
+          {count > 8 && (
             <itemS.ScrollbarContainer>
-              <itemS.ScrollTopArrow src='/img/scroll-top-arrow.svg' alt='화살표' />
+              <itemS.ScrollTopArrow
+                src="/img/scroll-top-arrow.svg"
+                alt="화살표"
+              />
               <itemS.ScrollbarWrapper
                 ref={scrollRef}
                 // onScroll={handleScrollSync}
@@ -183,13 +191,16 @@ export default function MyInquiryTable({ items, inquiryCount, isMemberMatch, fet
                   onMouseDown={handleThumbDrag}
                 />
               </itemS.ScrollbarWrapper>
-              <itemS.ScrollBottomArrow src='/img/scroll-bottom-arrow.svg' alt='화살표' />
+              <itemS.ScrollBottomArrow
+                src="/img/scroll-bottom-arrow.svg"
+                alt="화살표"
+              />
             </itemS.ScrollbarContainer>
-          }
+          )}
         </itemS.TableContainerWrapper>
       </itemS.Table>
       <itemS.ButtonContainer>
-        {isMemberMatch && 
+        {isMemberMatch && (
           <itemS.AllCheckBox>
             <itemS.AllCheck
               type="checkbox"
@@ -198,16 +209,19 @@ export default function MyInquiryTable({ items, inquiryCount, isMemberMatch, fet
               onChange={handleAllCheckChange}
             />
             <itemS.AllCheckText>전체 선택</itemS.AllCheckText>
-          </itemS.AllCheckBox>}
+          </itemS.AllCheckBox>
+        )}
         {isMemberMatch && (
           <itemS.ButtonBox>
-            <itemS.DeleteButton onClick={() => handleDelete(checkedItems)}>삭제</itemS.DeleteButton>
-            <itemS.WriteButton onClick={handleWriteClick}>글쓰기</itemS.WriteButton>
+            <itemS.DeleteButton onClick={() => handleDelete(checkedItems)}>
+              삭제
+            </itemS.DeleteButton>
+            <itemS.WriteButton onClick={handleWriteClick}>
+              글쓰기
+            </itemS.WriteButton>
           </itemS.ButtonBox>
         )}
-        
       </itemS.ButtonContainer>
-      
     </itemS.Container>
   );
 }

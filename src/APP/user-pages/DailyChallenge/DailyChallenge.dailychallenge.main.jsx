38640d8ae314cfee92f371489573c4600de69115
nunomi0 <ykys1@naver.com>
2025-07-08 20:32:53 +0900

    feat: 데일리 챌린지 메인 페이지 파일 생성

---
+++ src/APP/user-pages/DailyChallenge/DailyChallenge.dailychallenge.main.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import { useParams, useLocation } from 'react-router-dom';
+import * as Styled from './Styled/DailyChallenge.dailychallenge.main';
+import request from '../../Api/request';
+
+export default function WritePost() {
+  const location = useLocation();
+
+  return (
+    <Styled.Container>
+      
+    </Styled.Container>
+  );
+}
